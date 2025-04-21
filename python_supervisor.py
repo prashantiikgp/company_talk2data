68ec4e76a4f1449cd7fa30bc8d5d5d0e3dc7e6b4
Prashant <prashant.mine@gmail.com>
2025-04-21 23:42:55 +0530

    first -commit

---
+++ python_supervisor.py
@@ -0,0 +1,84 @@
+# %%
+#%pip install langgraph-supervisor langchain-openai langchain
+
+# %%
+#Initialize the Model
+from dotenv import load_dotenv
+import os 
+from langchain_openai import ChatOpenAI
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+model = ChatOpenAI(model="gpt-4o", openai_api_key=openai_api_key)
+
+# %%
+#Initialize and the tools
+
+def add(a: float, b: float) -> float:
+    """Add two numbers."""
+    return a + b
+
+def multiply(a: float, b: float) -> float:
+    """Multiply two numbers."""
+    return a * b
+
+def web_search(query: str) -> str:
+    """Search the web for information."""
+    return (
+        "Here are the headcounts for each of the FAANG companies in 2024:\n"
+        "1. **Facebook (Meta)**: 67,317 employees.\n"
+        "2. **Apple**: 164,000 employees.\n"
+        "3. **Amazon**: 1,551,000 employees.\n"
+        "4. **Netflix**: 14,000 employees.\n"
+        "5. **Google (Alphabet)**: 181,269 employees."
+    )
+
+
+# %%
+#Initialize the Agents
+from langgraph.prebuilt import create_react_agent
+math_agent = create_react_agent(
+    model=model,
+    tools=[add, multiply],
+    name="math_expert",
+    prompt="You are a math expert. Always use one tool at a time."
+)
+
+research_agent = create_react_agent(
+    model=model,
+    tools=[web_search],
+    name="research_expert",
+    prompt="You are a world class researcher with access to web search. Do not do any math."
+)
+
+
+# %%
+#Creater Supervisor 
+from langgraph_supervisor import create_supervisor
+
+# Create supervisor workflow
+workflow = create_supervisor(
+    [research_agent, math_agent],
+    model=model,
+    prompt=(
+        "You are a team supervisor managing a research expert and a math expert. "
+        "For current events, use research_agent. "
+        "For math problems, use math_agent."
+    )
+)
+
+
+# %%
+# Compile and run
+app = workflow.compile()
+result = app.invoke({
+    "messages": [
+        {
+            "role": "user",
+            "content": "what's the combined headcount of the FAANG companies in 2024?"
+        }
+    ]
+})
+
+print(result)
